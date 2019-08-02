954f7b5cd4ef6d3649736578828168708f6ef485
mrZ <mrzlab630@gmail.com>
2019-08-02 10:27:26 +0300

    Lets Rock

---
+++ docs/precache-manifest.0c6636abb6e649150a4ef0e0f4a954a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e908de390173802cee5c12ffe32f643b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9fb48f88ffeca9784cb",
+    "url": "/static/css/main.49a69e55.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a9fb48f88ffeca9784cb",
+    "url": "/static/js/main.9c804894.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
